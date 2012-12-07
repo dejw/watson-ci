bb1f38a4eefb9a1b3f8ca1bb377d54aa069ed9a6
Dawid Fatyga <dawid.fatyga@gmail.com>
2012-12-07 22:32:24 +0100

    Added tests for client.py

--- watson/client_test.py
+++ watson/client_test.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import __builtin__
 import mox
 import path
+import yaml
 
 from . import core
+from . import client
 from . import test_helper
 from .test_helper import unittest
 
@@ -11,7 +14,30 @@ from .test_helper import unittest
 class TestWatsonClient(test_helper.TestBase):
 
     def test_watch(self):
-        pass
+        working_dir = path.path(__file__).dirname() / '../fixtures/project1'
+
+        config = yaml.load(open(working_dir / core.CONFIG_FILENAME))
+        config['name'] = 'project1'
+        config['script'] = [config['script']]
+
+        cl = client.WatsonClient()
+        cl.add_project = self.mox.CreateMockAnything()
+        cl.add_project(working_dir, config)
+
+        self.mox.ReplayAll()
+
+        # when...
+        cl.watch(working_dir)
+
+        self.mox.VerifyAll()
+
+    def test_watch_raise_WatsonError_without_config(self):
+        cl = client.WatsonClient()
+        working_dir = (path.path(__file__).dirname()
+                       / '../fixtures/project_no_config')
+
+        with self.assertRaises(core.WatsonError):
+            cl.watch(working_dir)
 
 
 if __name__ == '__main__':
